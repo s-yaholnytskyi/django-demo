011d0e680605bd4a877c44a74a8e95a94ab6dd83
s-yaholnytskyi <s.yaholnytskyi@gmail.com>
2019-06-23 15:06:16 +0300

    added basic templates

--- django_demo/blog/views.py
+++ django_demo/blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts = [
+    {
+        'author': 'SerhiiY',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 23 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 23 2019'
+    }
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
