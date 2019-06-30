1bf0db0d3d0c5e20d93f6ce5006c526d909ae952
s-yaholnytskyi <s.yaholnytskyi@gmail.com>
2019-06-30 22:39:08 +0300

    added creation view and post detail view

--- django_demo/blog/views.py
+++ django_demo/blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView
+)
 from .models import Post
 
 
@@ -19,6 +24,15 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
