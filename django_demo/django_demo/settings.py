55b40d57e7b61fae0fb0e2f46f9fea9e89c27966
s-yaholnytskyi <s.yaholnytskyi@gmail.com>
2019-07-14 21:47:13 +0300

    reverting

--- django_demo/django_demo/settings.py
+++ django_demo/django_demo/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -150,4 +149,3 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-django_heroku.settings(locals())
